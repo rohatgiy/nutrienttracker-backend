94740c0e3f698f2178f7a841fea94f31754d2505
Yash Rohatgi <rohatgiy@gmail.com>
2020-08-14 15:08:13 -0400

    Added all routes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { request } = require('express');
 var router = express.Router();
 
 PORT = process.env.PORT || 5000;
@@ -11,9 +12,15 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use('/', router);
 app.use('/add', router);
+app.use('/dashboard', router);
+app.use('/history', router);
+app.use('/login', router);
+app.use('/createacc', router);
 
 /*
     all routes:
+        / - home 
+                methods: GET
         /add - add a food to today's entry
             methods: GET, POST
         /dashboard - shows today's nutrients
@@ -24,19 +31,55 @@ app.use('/add', router);
             methods: GET, POST 
         /createacc - create account
             methods: GET, POST
-        / - home 
-            methods: GET
 */
 
+// --> /
 router.get('/', (req, res, next) => {
     res.send('this is the home page');
 });
 
+// --> /add
 router.get('/add', (req, res, next) => {
     res.send('this is where you can add foods to today\'s entry');
 });
 
 router.post('/add', (req, res, next) => {
+    res.json(req.body);
+});
+
+// --> /dashboard
+router.get('/dashboard',  (req, res, next) => {
+    res.send('view today\'s nutrients');
+});
+
+router.post('/dashboard', (req, res, next) => {
+    res.json(request.body);
+});
+
+// --> /history
+router.get('/history', (req, res, next) => {
+    res.send('view the history of your nutrients');
+});
+
+router.post('/history', (req, res, next) => {
+    res.json(req.body);
+});
+
+// --> /login
+router.get('/login', (req, res, next) => {
+    res.send('login here');
+});
+
+router.post('/login', (req, res, next) => {
+    res.json(res.body);
+});
+
+// --> /createacc
+router.get('/createacc', (req, res, next) => {
+    res.send('create an account here');
+});
+
+router.post('/createacc', (req, res, next) => {
     res.json(req.body);
 });
 
