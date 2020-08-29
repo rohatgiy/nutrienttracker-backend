14bf6d1cf19da185342dd6f6d7bda734a9e4fbbe
Yash Rohatgi <rohatgiy@gmail.com>
2020-08-28 22:03:32 -0400

    history and dashboard routes work completely now

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -19,7 +19,6 @@ router.get('/',  (req, res, next) => {
     {
         res.redirect('/login');
     }
-    
 });
 
 module.exports = router;
--- routes/history.js
+++ routes/history.js
@@ -6,15 +6,6 @@ var date = new Date();
 var history = [];
 
 router.get('/', (req, res, next) => {
-    // var date = new Date();
-    // var today = new Date(date.getFullYear(), date.getMonth(), date.getDate());
-    // Entry.find({date: {$ne: today}}).then((doc) => {
-    //     res.send(doc);
-    // }).catch((err) =>
-    // {
-    //     console.log("error: " + err);
-    // })
-
     if (req.user)
     {
         for (i = req.user.entries.length-1 ; i >= 0; --i)
