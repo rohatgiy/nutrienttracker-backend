b2596b0d3d87eddedbbd57d046ee1c91df50f171
Yash Rohatgi <rohatgiy@gmail.com>
2020-08-30 16:41:50 -0400

    editacc route now lets user update their age and gender should that information change

--- routes/editacc.js
+++ routes/editacc.js
@@ -6,14 +6,32 @@ router.get('/', (req, res, next) => {
         2. fetch the user's info from db
         3. allow user to update their info
     */
-    res.send('this is where you can update your user information');
-})
+
+    if (req.user)
+    {
+        res.send('this is where you can update your user information');
+    }
+    else
+    {
+        res.redirect('/login');
+    }
+});
 
 router.post('/', (req, res, next) => {
-    /*
+     /*
         1. send the updated info to the db
     */
-    res.json(req.body);
-})
+    if (req.user)
+    {
+        req.user.age = req.body.age;
+        req.user.gender = req.body.gender;
+        req.user.save();
+        res.json(req.user);
+    }
+    else
+    {
+        res.redirect('/login');
+    }
+});
 
 module.exports = router;
