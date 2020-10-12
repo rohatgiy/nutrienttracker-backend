5da079331ec741ad140bbe8d229ef5df555541ac
Yash Rohatgi <rohatgiy@gmail.com>
2020-10-12 18:56:20 -0400

    working on editing food entries

--- routes/edit.js
+++ routes/edit.js
@@ -1,19 +1,63 @@
 const router = require('express').Router();
 
-router.get('/:id', (req, res, next) => {
+var replaceAndUpdate = function (req, res, next)
+{
+    var entries = req.user.entries;
+    var date = new Date();
+
+    if (entries.length > 0 && entries[entries.length-1].date.getTime() === 
+    new Date(date.getFullYear(), date.getMonth(), date.getDate()).getTime())
+    {
+        var originalEntry = entries[entries.length-1];
+    }
+    else
+    {
+       res.send('add an entry for today!')
+    }
+    
+    next();
+}
+
+router.get('/', (req, res, next) => {
     /*
-        1. get the entry with the corresponding id from db
+        1. get the entry with today's date from the db
         2. allow user to update entry
     */
-    res.send(`This is where you can edit the entry with id ${req.params.id}`);
 
+    if (!req.user)
+    {
+        res.redirect('/login');
+    }
+    else
+    {
+        var entries = req.user.entries;
+        var date = new Date();
+
+        if (entries.length > 0 && entries[entries.length-1].date.getTime() === 
+        new Date(date.getFullYear(), date.getMonth(), date.getDate()).getTime())
+        {
+            res.json(entries[entries.length-1]);
+        }
+        else
+        {
+            res.send('add an entry for today!')
+        }
+    }
 });
 
-router.post('/:id', (req, res, next) => {
+router.post('/', (req, res, next) => {
     /*
         1. send the updates to the entry to the db, and update the entry in the user's profile
     */
-    res.json(req.body);
+    
+   if (!req.user)
+   {
+       res.redirect('/login');
+   }
+   else
+   {
+       
+   }
 })
 
 module.exports = router;
